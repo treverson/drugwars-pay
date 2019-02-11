a039f4271c92f9ef749f2be3636bf743ef5f6aa9
Hightouch <cetinkaya.ozden@gmail.com>
2019-02-11 01:07:52 +0100

    Update db.js

--- helpers/db.js
+++ helpers/db.js
@@ -6,11 +6,11 @@ const Promise = require('bluebird');
 Promise.promisifyAll([Pool, Connection]);
 
 const db = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USERNAME,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DB,
-  connectionLimit: 15,
+  host: process.env.PROD_MYSQL_HOST,
+  user: process.env.PROD_MYSQL_USERNAME,
+  password: process.env.PROD_MYSQL_PASSWORD,
+  database: process.env.PROD_MYSQL_DB,
+  connectionLimit: 50,
   multipleStatements: true,
 });
 
